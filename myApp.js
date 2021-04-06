33f67df8efe1c78918402e347f41750bd5cf7579
Kevin <kevin.r.quito@gmail.com>
2021-04-06 17:35:19 +0100

    Put the app.get request in myApp.js instead of server.js

--- myApp.js
+++ myApp.js
@@ -2,16 +2,21 @@ var express = require('express');
 var app = express();
 var bGround = require('fcc-express-bground');
 
+// part 1
 bGround.log('Hello World');
 console.log('Hello World');
 
+// part 2
+/*
+function(req, res) {
+  res.send('Response String');
+}
+*/
+app.get("/", function(request, response){
+  response.send("Hello Express");
+});
 
-
-
-
-
-
-
+// part 3
 
 
 
