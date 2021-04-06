b1f1016f5072cc7c670203211ca0de336e209fe1
Kevin <kevin.r.quito@gmail.com>
2021-04-06 17:51:46 +0100

    Sending index.html file as a response to GET requests to the / path

--- myApp.js
+++ myApp.js
@@ -2,23 +2,32 @@ var express = require('express');
 var app = express();
 var bGround = require('fcc-express-bground');
 
-// part 1
+// meet the node console part 1
 bGround.log('Hello World');
 console.log('Hello World');
 
-// part 2
+// start a working express server part 2
 /*
+example ------- app.METHOD(PATH, HANDLER)
 function(req, res) {
   res.send('Response String');
 }
 */
+// app.get("/", function(request, response){
+//   response.send("Hello Express");
+// });
+
+// serve an html file part 3
+/*
+example -------absolutePath = __dirname + relativePath/file.ext
+function(req, res) {
+  res.sendFile(path);
+}
+*/
+
 app.get("/", function(request, response){
-  response.send("Hello Express");
+  response.sendFile(__dirname + "/views/index.html");
 });
-
-// part 3
-
-
 
 
 
