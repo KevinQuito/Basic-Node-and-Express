c516985a0983f110efd606a19c5aa068bbc36e83
Kevin <kevin.r.quito@gmail.com>
2021-04-06 19:03:29 +0100

    Create a simple API by creating a route that responds with JSON at the path /json

--- myApp.js
+++ myApp.js
@@ -35,11 +35,29 @@ app.get("/", function(request, response){
 /*
 example --------- express.static(absolutePath)
 app.use(path, middlewareFunction)
+
+NOTE: Our HTML should look better since the index.html is referencing the /public/style.css
+      and we're using app.use to serve the static assets. Basically, this allows index.HTML
+      to access the /public/style.css files.
+
+EXTRA NOTE: adding /public/style.css to the root address will allow you to see the css code
+            for example, https://salty-refuge-39989.herokuapp.com/public/style.css
 */
 app.use("/public", express.static(__dirname + "/public"));
 
 
+// serve json on a specific route part 5
+/*
+create a simple API by creating a route that responds with JSON at the path /json.
+use the method res.json(), passing in an object as an argument
+A valid object has the usual structure {key: data}
+data can be a number, a string, a nested object or an array
+data can also be a variable or the result of a function call, in which case it will be evaluated before being converted into a string.
+*/
 
+app.get("/json", function(request, response){
+  response.json({"message": "Hello json"});
+});
 
 
 
