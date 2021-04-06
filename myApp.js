7b6ddd42a1eb5954cbd57b16d6d6175416ed9723
Kevin <kevin.r.quito@gmail.com>
2021-04-06 18:37:58 +0100

    Mounts the express.static() middleware to the path /public with app.use()

--- myApp.js
+++ myApp.js
@@ -23,12 +23,20 @@ example -------absolutePath = __dirname + relativePath/file.ext
 function(req, res) {
   res.sendFile(path);
 }
+NOTE: This will allow you to connect to the /views/index.html and execute all the html code in there
 */
 
 app.get("/", function(request, response){
   response.sendFile(__dirname + "/views/index.html");
 });
 
+
+// serve static assets part 4
+/*
+example --------- express.static(absolutePath)
+app.use(path, middlewareFunction)
+*/
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
